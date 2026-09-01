ddffb1d98f9ada408e7def40448dfa28b9bc9c92
Clifferto <ferrarisdomingo17@gmail.com>
2026-09-01 17:37:45 -0300

    fixed minor issues in golay decoder model, added tests (success)

--- model/classes/GolayDecoder.py
+++ model/classes/GolayDecoder.py
@@ -24,56 +24,87 @@ class GolayDecoder:
             [1,1,1,1,0,0,0,1,1,0,1,0]   ,
             [1,1,1,0,1,0,1,0,1,0,0,1]]  , dtype=np.uint8)
 
-    def decode(self, r):
-        r = self.correct(r)
+    def decode(self, r, full_codeword = False):
+        """
+        Get info bits of given vector. Correction is made if applicable
 
-        if r is None:
-            word = None
-        else:
-            word = r[0:self._k]
+        Args:
+            r: Given received vector
+            full_codeword: True to return full corrected vector, False to return only info bits. Defaults to False.
 
-        return word
+        Returns:
+            tuple: (info_bits, corrected_flag, uncorrectable_flag)
+        """
+        r, corrected, uncorrectable = self.correct(r)
+        word                        = r if full_codeword else r[0:self._k]
+        return word, corrected, uncorrectable
 
     def correct(self, r):
-        r_low   = r[0:self._k]
-        r_high  = r[self._k::]
+        """
+        Correct given vector if applicable and return status flags. If uncorrectable received vector is unchanged
+
+        Args:
+            r: Given received vector
+
+        Returns:
+            tuple: (corrected_vector, corrected_flag, uncorrectable_flag)
+        """
+        r_low           = r[0:self._k]
+        r_high          = r[self._k::]
+        corrected       = False
+        uncorrectable   = False
 
         s = self._gf.mat_mul(r_low, self._G2412B) ^ r_high
         q = self._gf.mat_mul(r_high, self._G2412B) ^ r_low
         
         if self._gf.do_pack(s) != 0:
             error = self.get_error(s, q)
+
             if error is None:
-                r = None
+                # r, remains uncorrected
+                uncorrectable = True
             else:
-                r = r ^ error
-        return r
+                r           = r ^ error
+                corrected   = True
+
+        return r, corrected, uncorrectable
     
     def get_error(self, s, q):
+        """
+        Implements the golay 24,12 decoder 4 cases
+
+        Args:
+            s: Given syndrome
+            q: Given q
+
+        Returns:
+            np.array: Error pattern or None
+        """
         error_low   = None
         error_high  = None
 
         # all errors in parity
         if self._gf.hamming_weight(s) <= 3:
-            error_low   = np.zeros(self._k)
+            error_low   = self._gf.do_unpack(0, self._k)
             error_high  = s
         # all errors in word
-        elif self._gf.hamming_weight(q) <=3:
+        elif self._gf.hamming_weight(q) <= 3:
             error_low   = q
-            error_high  = np.zeros(self._k)
+            error_high  = self._gf.do_unpack(0, self._k)
         else:
             for i, bi in enumerate(self._G2412B):
                 sbi = s ^ bi
                 qbi = q ^ bi
+                ui  = self._gf.do_unpack(1<<i, self._k)[::-1]
                 # only one error in word, remaining in parity
-                if self._gf.hamming_weight(sbi) <=2:
-                    error_low   = np.zeros(self._k); error_low[i] = 1
+                if self._gf.hamming_weight(sbi) <= 2:
+                    error_low   = ui
                     error_high  = sbi
                     break
                 # only one error in parity remaining in word
                 elif self._gf.hamming_weight(qbi) <= 2:
                     error_low   = qbi
-                    error_high  = np.zeros(self._k); error_high[i] = 1
+                    error_high  = ui
                     break
 
         # if more than one error in word and parity also, then errors > 3, uncorrectable
