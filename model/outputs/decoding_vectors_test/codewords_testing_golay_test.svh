059474e0021836f5ed80e1375f9df7773116ac92
AbelCorvalan0 <abel.corvalan@mi.unc.edu.ar>
2026-09-16 15:17:49 -0300

    .svh files vectors written as sv vectors

--- model/outputs/decoding_vectors_test/codewords_testing_golay_test.svh
+++ model/outputs/decoding_vectors_test/codewords_testing_golay_test.svh
@@ -1,27 +1,30 @@
-	constraint golay_code_test_vectors {
-    //0xA5D9A6, 0xA5F9A4, 0xA5C9AA 
-		rx_data inside {
-			24'b101001011101100110100110	,
-			24'b101001011111100110100100	,
-			24'b101001011100100110101010	};
+	// Selected received test vectors: 0xA5D9A6, 0xA5F9A4, 0xA5C9AA
+	bit [NB_CODEWORD-1:0] rx_test_vectors [3] = '{
+		24'b101001011101100110100110	,
+		24'b101001011111100110100100	,
+		24'b101001011100100110101010	};
 
-		decoded_data inside {
-			12'b101001011100	,
-			12'b101001011100	,
-			12'b101001011100	};
+	// Decoded message for the selected test vectors
+	bit [NB_WORD-1:0] msg_test_vectors [3] = '{
+		12'b101001011100	,
+		12'b101001011100	,
+		12'b101001011100	};
 
-		err inside {
-			24'b000000000001000000000011	,
-			24'b000000000011000000000001	,
-			24'b000000000000000000000000	};
+	// Recovered error pattern for the selected test vectors
+	bit [NB_CODEWORD-1:0] err_pattern_test_vectors [3] = '{
+		24'b000000000001000000000011	,
+		24'b000000000011000000000001	,
+		24'b000000000000000000000000	};
 
-		corrected_data inside {
-			1'b1	,
-			1'b1	,
-			1'b0	};
+	// Corrected flag for the selected test vectors
+	bit corrected_flag_test_vectors [3] = '{
+		1'b1	,
+		1'b1	,
+		1'b0	};
 
-		uncorrectable inside {
-			1'b0	,
-			1'b0	,
-			1'b1	};
-	};
+	// Uncorrectable flag for the selected test vectors
+	bit uncorrectable_flag_test_vectors [3] = '{
+		1'b0	,
+		1'b0	,
+		1'b1	};
+
