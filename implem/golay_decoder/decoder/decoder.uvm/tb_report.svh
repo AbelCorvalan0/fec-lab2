1a9c012eb64d3a40c0a03982df366f529d32a163
Clifferto <ferrarisdomingo17@gmail.com>
2026-09-23 22:04:22 -0300

    minor fix to report table generation for decoder test (success)

--- implem/golay_decoder/decoder/decoder.uvm/tb_report.svh
+++ implem/golay_decoder/decoder/decoder.uvm/tb_report.svh
@@ -3,9 +3,9 @@ class tb_report extends uvm_object;
     localparam MAX_ERROR_WEIGHT = 4;
 
     // array index represent error weight, ranges from 0 to 4
-    int unsigned error_weight   [MAX_ERROR_WEIGHT+1]    = '{MAX_ERROR_WEIGHT+1{0}};
-    int unsigned corrected      [MAX_ERROR_WEIGHT+1]    = '{MAX_ERROR_WEIGHT+1{0}};
-    int unsigned detected       [MAX_ERROR_WEIGHT+1]    = '{MAX_ERROR_WEIGHT+1{0}};
+    int unsigned error_weight   [MAX_ERROR_WEIGHT+1]    = '{(MAX_ERROR_WEIGHT+1){0}};
+    int unsigned corrected      [MAX_ERROR_WEIGHT+1]    = '{(MAX_ERROR_WEIGHT+1){0}};
+    int unsigned detected       [MAX_ERROR_WEIGHT+1]    = '{(MAX_ERROR_WEIGHT+1){0}};
 
     `uvm_object_utils_begin(tb_report)
         `uvm_field_sarray_int(error_weight  , UVM_DEFAULT | UVM_DEC)
@@ -20,6 +20,10 @@ class tb_report extends uvm_object;
     function void update(seq_item item);
         int unsigned weight = $countones(item.error_pattern);
 
+        // uncorrectable will give zero error_pattern
+        if (item.uncorrectable) begin
+            weight = 4;
+        end
         error_weight[weight]++;
         corrected[weight]   += item.corrected;
         detected[weight]    += item.uncorrectable;
